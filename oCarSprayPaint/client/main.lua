e0259f617808304a2ed1e020053e73192783c273
DEN <68790210+DENZEAM@users.noreply.github.com>
2024-06-06 20:59:35 +0200

    chore: Update Car Spray Paint client script

--- oCarSprayPaint/client/main.lua
+++ oCarSprayPaint/client/main.lua
@@ -1,21 +1,68 @@
 -- ESX = nil
 -- Citizen.CreateThread(function()
 --     while ESX == nil do
---         TriggerEvent('esx:getSharedObject', function(obj) 
--- 			ESX = obj 
+--         TriggerEvent('esx:getSharedObject', function(obj)
+-- 			ESX = obj
 -- 		end)
 --         Citizen.Wait(100)
 --     end
---     while ESX.GetPlayerData().job == nil do 
+--     while ESX.GetPlayerData().job == nil do
 --         Citizen.Wait(100)
 --     end
 --     ESX.PlayerData = ESX.GetPlayerData()
 -- end)
 
-local LoadPoint = {".", "..", "...", ""}
+local LoadPoint = { ".", "..", "...", "" }
 local colorselected = ConfigDBP.ColorId[ConfigDBP.ColorName[1]]
 local ouiounon = 0
 local customindex = 1
+
+function PlaySprayCanAnimation(time)
+    local playerPed = PlayerPedId()
+    local sprayCanDict = "anim@scripted@freemode@postertag@graffiti_spray@male@"
+    local sprayCanAnim = "spray_can_var_01_male"
+    local sprayCanProp = "prop_cs_spray_can"
+    local particleDict = "core"
+    local particleName = "exp_grd_bzgas_smoke"
+
+    -- Charger le dictionnaire d'animation
+    RequestAnimDict(sprayCanDict)
+    while not HasAnimDictLoaded(sprayCanDict) do
+        Citizen.Wait(100)
+    end
+
+    -- Charger le dictionnaire de particules
+    RequestNamedPtfxAsset(particleDict)
+    while not HasNamedPtfxAssetLoaded(particleDict) do
+        Citizen.Wait(100)
+    end
+
+    -- Aligner l'orientation du joueur
+    local heading = GetEntityHeading(playerPed)
+    SetEntityHeading(playerPed, heading)
+
+    -- Jouer l'animation
+    TaskPlayAnim(playerPed, sprayCanDict, sprayCanAnim, 8.0, -8.0, -1, 1, 0, false, false, false)
+
+    -- Attacher le prop de la bombe de peinture au joueur
+    local x, y, z = table.unpack(GetEntityCoords(playerPed))
+    local prop = CreateObject(GetHashKey(sprayCanProp), x, y, z + 0.2, true, true, true)
+    AttachEntityToEntity(prop, playerPed, GetPedBoneIndex(playerPed, 28422), 0.0, 0.0, 0.0700, 0.0017365, 0.0, 0.0, true,
+        true, false, true, 1, true)
+
+    -- Commencer l'effet de particules
+    UseParticleFxAssetNextCall(particleDict)
+    local particleEffect = StartNetworkedParticleFxLoopedOnEntityBone(particleName, playerPed, 0.0, 0.0, 0.0, 0.0, 90.0,
+        0.0,
+        GetEntityBoneIndexByName(prop, "spray"), 1.4, false, false, false)
+
+    Citizen.Wait(time * 1000) -- Durée de l'animation
+
+    -- Arrêter l'effet de particules et l'animation, et supprimer l'objet après le temps spécifié
+    StopParticleFxLooped(particleEffect, 0)
+    ClearPedTasks(playerPed)
+    DeleteObject(prop)
+end
 
 function _U(entry)
     local locale = ConfigDBP.Locale
@@ -32,31 +79,31 @@ dmbombe.Closed = function()
 end
 
 function dbombe()
-	if open then
-		open = false
-		RageUI.Visible(dmbombe,false)
-		return
-	else
-		open = true
-		RageUI.Visible(dmbombe,true)
-		CreateThread(function()
-			while open do 
+    if open then
+        open = false
+        RageUI.Visible(dmbombe, false)
+        return
+    else
+        open = true
+        RageUI.Visible(dmbombe, true)
+        CreateThread(function()
+            while open do
                 local pcoords = GetEntityCoords(PlayerPedId())
                 local veh, dst = ESX.Game.GetClosestVehicle(pcoords)
                 local model = GetEntityModel(veh)
                 local nameveh = GetDisplayNameFromVehicleModel(model)
                 local plate = GetVehicleNumberPlateText(veh)
-                if ConfigDBP.BlackListVehicle[nameveh] then 
+                if ConfigDBP.BlackListVehicle[nameveh] then
                     ouiounon = 0
                 else
                     ouiounon = 1
                 end
-                if dst > 5 then 
+                if dst > 5 then
                     RageUI.CloseAll()
                     ESX.ShowNotification(_U('no_vehicle'))
                     return
                 end
-				RageUI.IsVisible(dmbombe,function() 
+                RageUI.IsVisible(dmbombe, function()
                     RageUI.Separator(_U('selected_vehicle') .. nameveh)
                     RageUI.Separator(_U('vehicle_plate') .. plate)
                     if ouiounon == 1 then
@@ -64,23 +111,24 @@ function dbombe()
                     else
                         RageUI.Separator(_U('custom_not_possible'))
                     end
-                    RageUI.List("Couleur", ConfigDBP.ColorName, customindex, nil, {}, ouiounon == 1,{
+                    RageUI.List("Couleur", ConfigDBP.ColorName, customindex, nil, {}, ouiounon == 1, {
                         onListChange = function(index)
                             customindex = index
                             colorselected = ConfigDBP.ColorId[ConfigDBP.ColorName[index]]
                         end
                     })
-                    RageUI.Button(_U('confirm_modification'), nil, {RightBadge = RageUI.BadgeStyle.Tick}, ouiounon == 1, {
-						onSelected = function()
-                            RageUI.CloseAll()
-							vehiclemodif(veh,colorselected,colorselected)
-						end
-					})
-				end)
-				Citizen.Wait(1)
-			end
-		end)
-	end
+                    RageUI.Button(_U('confirm_modification'), nil, { RightBadge = RageUI.BadgeStyle.Tick }, ouiounon == 1,
+                        {
+                            onSelected = function()
+                                RageUI.CloseAll()
+                                vehiclemodif(veh, colorselected, colorselected)
+                            end
+                        })
+                end)
+                Citizen.Wait(1)
+            end
+        end)
+    end
 end
 
 function DrawTextProgress(Text, Text3, Taille, Text2, Font, Justi, havetext)
@@ -121,7 +169,7 @@ function AddProgressBar(Text, r, g, b, a, Timing)
     end
 end
 
-function DeleteProgressBar() 
+function DeleteProgressBar()
     ActiveProgressBar = nil
 end
 
@@ -133,9 +181,7 @@ end)
 function vehiclemodif(vehicle, color1, color2)
     TriggerServerEvent("DEN:RemoveItem")
     AddProgressBar(_U('action_in_progress'), 42, 37, 40, 0.74, ConfigDBP.Time * 1000)
-    TaskStartScenarioInPlace(PlayerPedId(), 'CODE_HUMAN_MEDIC_KNEEL', -1, false)
-    Wait(ConfigDBP.Time * 1000)
-    ClearPedTasks(PlayerPedId())
+    PlaySprayCanAnimation(ConfigDBP.Time)
     ESX.Game.SetVehicleProperties(vehicle, {
         color1 = color1,
         color2 = color2
